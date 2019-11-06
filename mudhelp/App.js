761ffe9c6d81e2c79179b9620013153ae5bd7a80
Wagner <aewinformatica@gmail.com>
2019-11-06 18:36:50 +0000

    inclusao de imagens e API GOOGLE MAPS

--- mudhelp/App.js
+++ mudhelp/App.js
@@ -1,19 +1,7 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+
+import Index from "./src";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello AewInformatica</Text>
-    </View>
-  );
+  return <Index />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mudhelp/src/components/Details/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+import {
+  Container,
+  TypeTitle,
+  TypeDescription,
+  TypeImage,
+  RequestButton,
+  ResquestButtonText
+} from "./styles";
+
+import uberx from "../../../assets/uberx.png";
+
+export default class Details extends Component {
+  render() {
+    return (
+      <Container>
+        <TypeTitle> Popular </TypeTitle>
+        <TypeDescription> Viagens baratas para o transporte do seus objetos do dia a dia </TypeDescription>
+        <TypeImage source={uberx} />
+        <TypeTitle>MudHelpX</TypeTitle>
+        <TypeDescription>R$6,00 </TypeDescription>
+
+        <RequestButton onPress={() => {}}>
+          <ResquestButtonText>Solicitar MudHelp</ResquestButtonText>
+        </RequestButton>
+      </Container>
+    );
+  }
+}
